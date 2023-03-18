e089767cdd2222842701780e85be071aeaff1321
Veronika Genkinova <[veranika-2005@ukr.net]>
2023-03-18 16:06:44 +0200

    1

---
+++ src/main/java/com/hillel/homework19/Calculator.java
@@ -0,0 +1,21 @@
+package com.hillel.homework19;
+
+
+import org.hillel.calculator.Calculation;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Calculator {
+}
